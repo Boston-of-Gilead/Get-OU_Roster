9356c0d5ac900116ae709607351f7209f1017b44
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-15 11:09:42 -0400

    Add files via upload

---
+++ Get_OU_Roster.ps1
@@ -0,0 +1,4 @@
+﻿#Gets all users in OU
+$OUpath = 'OU=,DC=,DC=,DC=local' 
+$ExportPath = 'C:\path_to\users_in_ou2.csv' 
+Get-ADUser -Filter * -SearchBase $OUpath | Select-object Name,UserPrincipalName,EmployeeNumber | Export-Csv -NoType $ExportPath
